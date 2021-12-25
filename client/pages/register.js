100eee41fa32a8ad4710adad51a133434a923ba4
gassdev <gassdev@gmail.com>
2021-12-25 01:35:26 +0000

    add register user feature, install react-toastify and add custom next dev server for development mode

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,20 +1,32 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { toast } from 'react-toastify'
+import { SyncOutlined } from '@ant-design/icons'
+import Link from 'next/link'
 
 const Register = () => {
   const [name, setName] = useState(process.env.NEXT_PUBLIC_name)
   const [email, setEmail] = useState(process.env.NEXT_PUBLIC_email)
   const [password, setPassword] = useState(process.env.NEXT_PUBLIC_password)
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     // console.table(name, email, password)
-    const { data } = await axios.post('http://localhost:8000/api/register', {
-      name,
-      email,
-      password,
-    })
-    console.log(`REGISTER RESPONSE ---> ${data}`)
+    try {
+      setLoading(true)
+      const { data } = await axios.post(`/api/register`, {
+        name,
+        email,
+        password,
+      })
+      // console.log('REGISTER RESPONSE ---> ', data)
+      toast.success('Regitration successful. Please login.')
+      setLoading(false)
+    } catch (err) {
+      toast.error(err.response.data)
+      setLoading(false)
+    }
   }
   return (
     <>
@@ -43,10 +55,20 @@ const Register = () => {
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Entrer votre mot de passe"
           />
-          <button className="btn btn-block btn-outline-primary p-2">
-            S'inscrire
+          <button
+            className="btn btn-block btn-outline-primary p-2"
+            disabled={!name || !email || !password || loading}
+          >
+            {loading ? <SyncOutlined spin /> : "S'inscrire"}
           </button>
         </form>
+
+        <p className="text-center p-3">
+          Already registered?{' '}
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </p>
       </div>
     </>
   )
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,38 @@
-export const register = (req, res) => {
-  console.log(req.body)
-  res.json('register user response')
+import User from '../models/user'
+import { hashPassword, comparePassword } from '../utils/auth'
+
+export const register = async (req, res) => {
+  try {
+    // console.log(req.body)
+    const { name, email, password } = req.body
+    // validation
+    if (!name) return res.status(400).send('Name is required')
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send('Password is required and should be minimum 6 characters long')
+    }
+
+    let userExist = await User.findOne({ email }).exec()
+    if (userExist) {
+      return res.status(400).send('That email is already taken')
+    }
+
+    // hash password
+    const hashedPassword = await hashPassword(password)
+
+    // register user
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    })
+    await user.save()
+    console.log('save user ---> ', user)
+    // return res.json({ ok: true })
+    return res.json({ ok: true })
+  } catch (err) {
+    console.log(err)
+    return res.status(400).send('Error. Try again.')
+  }
 }
