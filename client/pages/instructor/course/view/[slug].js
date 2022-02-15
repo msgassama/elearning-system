31922180fb4b510183b57de56b49e0bef0c96fc2
gassdev <gassdev@gmail.com>
2022-02-15 00:29:06 +0000

    add lesson modal

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import InstructorRoute from '../../../../components/routes/InstructorRoute'
 import axios from 'axios'
-import { Avatar, Tooltip } from 'antd'
-import { EditOutlined, CheckOutlined } from '@ant-design/icons'
+import { Avatar, Tooltip, Button, Modal } from 'antd'
+import { EditOutlined, CheckOutlined, UploadOutlined } from '@ant-design/icons'
 import ReactMarkdown from 'react-markdown'
 
 const CourseView = () => {
   const [course, setCourse] = useState({})
+  // for lessons
+  const [visible, setVisible] = useState(false)
 
   const router = useRouter()
   const { slug } = router.query
@@ -62,6 +64,28 @@ const CourseView = () => {
                 <ReactMarkdown children={course.description} />
               </div>
             </div>
+            <div className="row">
+              <Button
+                onClick={() => setVisible(true)}
+                className="col-md-6 offset-md-3 text-center"
+                type="primary"
+                shape="round"
+                icon={<UploadOutlined />}
+                size="large"
+              >
+                Add Lesson
+              </Button>
+            </div>
+            <br />
+            <Modal
+              title="+ Add Lesson"
+              centered
+              visible={visible}
+              onCancel={() => setVisible(false)}
+              footer={null}
+            >
+              add lesson component
+            </Modal>
           </div>
         )}
       </div>
