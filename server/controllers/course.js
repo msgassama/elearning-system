12ff52727337df84f1fb0ea5a9fb346593bad24a
gassdev <gassdev@gmail.com>
2022-02-15 12:01:22 +0000

    upload video to S3 bucket

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -2,6 +2,8 @@ import AWS from 'aws-sdk'
 import { nanoid } from 'nanoid'
 import Course from '../models/course'
 import slugify from 'slugify'
+import { readFileSync } from 'fs'
+
 const awsConfig = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -102,6 +104,37 @@ export const read = async (req, res) => {
       .populate('instructor', '_id name')
       .exec()
     res.json(course)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const uploadVideo = async (req, res) => {
+  try {
+    const { video } = req.files
+    // console.log(video)
+    if (!video) {
+      return res.status(400).send('No video')
+    }
+
+    // video params
+    const params = {
+      Bucket: 'lmsacademybucket',
+      Key: `${nanoid()}.${video.type.split('/')[1]}`,
+      Body: readFileSync(video.path),
+      ACL: 'public-read',
+      ContentType: video.type,
+    }
+
+    // upload to S3
+    S3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err)
+        res.sendStatus(400)
+      }
+      console.log(data)
+      res.send(data)
+    })
   } catch (err) {
     console.log(err)
   }
