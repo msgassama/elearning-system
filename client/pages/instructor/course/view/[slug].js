22cfa4becb778d84ef1f68fc65012934934c2a29
gassdev <gassdev@gmail.com>
2022-02-16 00:31:18 +0000

    rendering lessons as list items

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import InstructorRoute from '../../../../components/routes/InstructorRoute'
 import axios from 'axios'
-import { Avatar, Tooltip, Button, Modal } from 'antd'
+import { Avatar, Tooltip, Button, Modal, List } from 'antd'
 import { EditOutlined, CheckOutlined, UploadOutlined } from '@ant-design/icons'
 import ReactMarkdown from 'react-markdown'
 import AddLessonForm from '../../../../components/forms/AddLessonForm'
 import { toast } from 'react-toastify'
+
+const { Item } = List
 
 const CourseView = () => {
   const [course, setCourse] = useState({})
@@ -175,6 +177,26 @@ const CourseView = () => {
                 handleVideoRemove={handleVideoRemove}
               />
             </Modal>
+
+            <div className="row pb-5">
+              <div className="col lesson-list">
+                <h4>
+                  {course && course.lessons && course.lessons.length} Lessons
+                </h4>
+                <List
+                  itemLayout="horizontal"
+                  dataSource={course && course.lessons}
+                  renderItem={(item, index) => (
+                    <Item>
+                      <Item.Meta
+                        avatar={<Avatar>{index + 1}</Avatar>}
+                        title={item.title}
+                      ></Item.Meta>
+                    </Item>
+                  )}
+                ></List>
+              </div>
+            </div>
           </div>
         )}
       </div>
