ba4b59fc70761d7c873db83922ea1fccea7e45f3
gassdev <gassdev@gmail.com>
2022-02-24 18:26:07 +0000

    get published courses on client side

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const Index = () => {
+  const [courses, setCourses] = useState([])
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const { data } = await axios.get('/api/courses')
+      setCourses(data)
+    }
+    fetchCourses()
+  }, [])
+
   return (
     <>
       <h1 className="jumbotron square text-center bg-primary">
         Online Education Plateform
       </h1>
-      <p>from next js...</p>
+      <div className="container-fluid">
+        <div className="row">
+          {courses.map((course) => (
+            <div key={course._id} className="col-md-4">
+              <pre>{JSON.stringify(course, null, 4)}</pre>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
