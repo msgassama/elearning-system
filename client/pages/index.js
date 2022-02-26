006907fd9d3631cc9b6de58cfc69ceb6925efd61
gassdev <gassdev@gmail.com>
2022-02-26 23:13:56 +0000

    add currencyFormatter and server render courses fetaures for SEO

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import CourseCard from '../components/cards/CourseCard'
 
-const Index = () => {
-  const [courses, setCourses] = useState([])
+const Index = ({ courses }) => {
+  // const [courses, setCourses] = useState([])
 
-  useEffect(() => {
-    const fetchCourses = async () => {
-      const { data } = await axios.get('/api/courses')
-      setCourses(data)
-    }
-    fetchCourses()
-  }, [])
+  // useEffect(() => {
+  //   const fetchCourses = async () => {
+  //     const { data } = await axios.get('/api/courses')
+  //     setCourses(data)
+  //   }
+  //   fetchCourses()
+  // }, [])
 
   return (
     <>
@@ -30,6 +30,15 @@ const Index = () => {
       </div>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(`${process.env.API}/courses`)
+  return {
+    props: {
+      courses: data,
+    },
+  }
 }
 
 export default Index
