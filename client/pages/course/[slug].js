d48d4f87768e4a92fdab3da722b21800b8b29e18
gassdev <gassdev@gmail.com>
2022-02-28 21:51:34 +0000

    fix "text content did not match warning" by using useEffect hook

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -1,33 +1,42 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SingleCourseJumbotron from '../../components/cards/SingleCourseJumbotron'
 
-const SingleCourse = ({ course }) => {
+const SingleCourse = ({ selectedCourse }) => {
   // state
   const [showModal, setShowModal] = useState(false)
   const [preview, setPreview] = useState('')
+  const [course, setCourse] = useState(undefined)
 
-  return (
-    <>
-      {/* <pre>{JSON.stringify(course, null, 4)}</pre> */}
-      <SingleCourseJumbotron
-        course={course}
-        showModal={showModal}
-        setShowModal={setShowModal}
-        preview={preview}
-        setPreview={setPreview}
-      />
+  useEffect(() => {
+    setCourse(selectedCourse)
+  }, [])
 
-      {showModal ? course.lessons[0].video.Location : "don't show"}
-    </>
-  )
+  if (course) {
+    return (
+      <>
+        {/* <pre>{JSON.stringify(course, null, 4)}</pre> */}
+        <SingleCourseJumbotron
+          course={course}
+          showModal={showModal}
+          setShowModal={setShowModal}
+          preview={preview}
+          setPreview={setPreview}
+        />
+
+        {showModal ? course.lessons[0].video.Location : "don't show"}
+      </>
+    )
+  } else {
+    return null
+  }
 }
 
 export async function getServerSideProps({ query }) {
   const { data } = await axios.get(`${process.env.API}/course/${query.slug}`)
   return {
     props: {
-      course: data,
+      selectedCourse: data,
     },
   }
 }
