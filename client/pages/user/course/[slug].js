ef260dc88724ff5bde02a8266fc047da37ae542e
gassdev <gassdev@gmail.com>
2022-05-11 13:32:47 +0000

    single course view for user

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import axios from 'axios'
+import user from '../../../../server/models/user'
 
 const SingleCourse = () => {
+  const [loading, setLoading] = useState(false)
+  const [course, setCourse] = useState({ lessons: [] })
+
+  // router
   const router = useRouter()
+
+  const { slug } = router.query
+
+  useEffect(() => {
+    if (slug) loadCourse()
+  }, [slug])
+
+  const loadCourse = async () => {
+    const { data } = await axios.get(`/api/course/${slug}`)
+    setCourse(data)
+  }
+
   return (
     <>
-      <h1>Couse slug is: {JSON.stringify(router.query.slug)}</h1>
+      <h1>Couse slug is: {JSON.stringify(course, null, 4)}</h1>
     </>
   )
 }
