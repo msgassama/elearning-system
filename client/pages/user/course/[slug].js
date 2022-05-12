b722b2a3bea0df6e071f8c98c5009e4d43ae776e
gassdev <gassdev@gmail.com>
2022-05-12 21:03:25 +0000

    display course lessons in menu sidebar and play clicked lesson video

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,9 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, createElement } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import StudentRoute from '../../../components/routes/StudentRoute'
+import { Button, Menu, Avatar } from 'antd'
+import ReactPlayer from 'react-player'
+import ReactMarkdown from 'react-markdown'
+import {
+  PlayCircleOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons'
+
+const { Item } = Menu
 
 const SingleCourse = () => {
+  const [clicked, setClicked] = useState(-1)
+  const [collapsed, setCollapsed] = useState(false)
   const [loading, setLoading] = useState(false)
   const [course, setCourse] = useState({ lessons: [] })
 
@@ -23,7 +35,65 @@ const SingleCourse = () => {
 
   return (
     <StudentRoute>
-      <h1>Couse slug is: {JSON.stringify(course, null, 4)}</h1>
+      <div className="row">
+        <div style={{ maxWidth: '320px' }}>
+          <Button
+            className="text-primarymt-1 btn-block mb-2"
+            onClick={() => setCollapsed(!collapsed)}
+          >
+            {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}{' '}
+            {!collapsed && 'Lessons'}
+          </Button>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={[clicked]}
+            inlineCollapsed={collapsed}
+            style={{ height: '80vh', overflow: 'scroll' }}
+          >
+            {course.lessons.map((lesson, index) => (
+              <Item
+                onClick={() => setClicked(index)}
+                key={index}
+                icon={<Avatar>{index + 1}</Avatar>}
+              >
+                {lesson.title.substring(0, 30)}
+              </Item>
+            ))}
+          </Menu>
+        </div>
+
+        <div className="col">
+          {clicked !== -1 ? (
+            <>
+              {course.lessons[clicked].video &&
+                course.lessons[clicked].video.Location && (
+                  <>
+                    <div className="wrapper">
+                      <ReactPlayer
+                        className="player"
+                        url={course.lessons[clicked].video.Location}
+                        width="100%"
+                        height="100%"
+                        controls
+                      />
+                    </div>
+                  </>
+                )}
+              <ReactMarkdown
+                children={course.lessons[clicked].content}
+                className="single-post"
+              />
+            </>
+          ) : (
+            <div className="d-flex justify-content-center p-5">
+              <div className="text-center p-5">
+                <PlayCircleOutlined className="text-primary display-1 p-5" />
+                <p className="lead">Click on the lessons to start learning</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </StudentRoute>
   )
 }
