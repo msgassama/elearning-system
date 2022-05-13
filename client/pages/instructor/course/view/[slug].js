6872af4b317e71a0220517f4924e7300a8d40d98
gassdev <gassdev@gmail.com>
2022-05-13 22:28:13 +0000

    enrolled students count feature

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -9,6 +9,7 @@ import {
   UploadOutlined,
   QuestionOutlined,
   CloseOutlined,
+  UserSwitchOutlined,
 } from '@ant-design/icons'
 import ReactMarkdown from 'react-markdown'
 import AddLessonForm from '../../../../components/forms/AddLessonForm'
@@ -29,6 +30,8 @@ const CourseView = () => {
   const [uploading, setUploading] = useState(false)
   const [uploadButtonText, setUploadButtonText] = useState('Upload Video')
   const [progress, setProgress] = useState(0)
+  // student count
+  const [students, setStudents] = useState(0)
 
   const router = useRouter()
   const { slug } = router.query
@@ -37,9 +40,21 @@ const CourseView = () => {
     loadCourse()
   }, [slug])
 
+  useEffect(() => {
+    course && studentCount()
+  }, [course])
+
   const loadCourse = async () => {
     const { data } = await axios.get(`/api/course/${slug}`)
     setCourse(data)
+  }
+
+  const studentCount = async () => {
+    const { data } = await axios.post(`/api/instructor/student-count`, {
+      courseId: course._id,
+    })
+    console.log('STUDENT COUNT ==> ', data)
+    setStudents(data.length)
   }
 
   // FUNCTIONS FOR ADD LESSON
@@ -170,6 +185,9 @@ const CourseView = () => {
                   </div>
 
                   <div className="d-flex pt-4">
+                    <Tooltip title={`${students} Enrolled`}>
+                      <UserSwitchOutlined className="h5 pointer text-info mr-4" />
+                    </Tooltip>
                     <Tooltip title="Edit">
                       <EditOutlined
                         onClick={() =>
